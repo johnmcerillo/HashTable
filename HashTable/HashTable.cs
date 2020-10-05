bba22e9a2d35905810231063741e2e55b505235c
johnmcerillo <johnmcerillo@gmail.com>
2020-10-05 19:39:58 -0400

    Adding Count property and comments

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 namespace HashTable
 {
+    /// <summary>
+    /// Hash table implemented using a circular array with chaining
+    /// to resolve collisions.
+    /// </summary>
+    /// <typeparam name="TKey">The key type.</typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
     public class HashTable<TKey, TValue>
     {
 
+        /// <summary>
+        /// The circular array.
+        /// </summary>
         private Entry[] m_Table = new Entry[8];
+
+        /// <summary>
+        /// The number of occupied entries in the array.
+        /// </summary>
         private int m_OccupiedCount;
 
+        /// <summary>
+        /// Gets the number of entries in the table.
+        /// </summary>
+        public int Count => m_OccupiedCount;
+
+        /// <summary>
+        /// Adds the <see cref="key"/>, <see cref="value"/> pair to the table.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
         public void Add(TKey key, TValue value)
         {
             if(0.87 < (m_OccupiedCount / (double)m_Table.Length))
@@ -35,17 +58,35 @@ namespace HashTable
             }
 
         }
-
+        /// <summary>
+        /// Determines whether or not the table contains <see cref="key"/>.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>True if the table contains <see cref="key"/>,
+        /// false otherwise.</returns>
         public bool ContainsKey(TKey key)
         {
             return 0 <= FindKeyIndex(key, true);
         }
 
+        /// <summary>
+        /// Gets the value associated with <see cref="key"/>.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>The value.</returns>
+        /// <exception cref="Exception">Thrown if <see cref="key"/>
+        /// cannot be found.</exception>
         public TValue GetValue(TKey key)
         {
             return m_Table[FindKeyIndex(key, false)].Value;
         }
 
+        /// <summary>
+        /// Removes <see cref="key"/>.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <exception cref="Exception">Thrown when the key is
+        /// not found.</exception>
         public void Remove(TKey key)
         {
             var indexKey = FindKeyIndex(key, false);
@@ -53,11 +94,22 @@ namespace HashTable
             m_OccupiedCount--;
         }
 
+        /// <summary>
+        /// Gets the index to the array location that <see cref="key"/> is
+        /// located at if its location has not been impacted by collisions.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="tableLen">The length of the circular array.</param>
+        /// <returns>The first guess index.</returns>
         private int GuessIndex(TKey key, int tableLen)
         {
             return Math.Abs(key.GetHashCode()) % tableLen;
         }
 
+        /// <summary>
+        /// Doubles the capacity of the circular array and re-adds all entries.
+        /// This is a costly operation.
+        /// </summary>
         private void Expand()
         {
             var tableOld = m_Table;
@@ -74,6 +126,17 @@ namespace HashTable
             }
         }
 
+        /// <summary>
+        /// Finds the index of <see cref="key"/> in the circular array. Accounts
+        /// for collisions by searching to the right from the index returned by
+        /// <see cref="GuessIndex(TKey, int)"/>.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="safe">True to suppress key not found exceptions.</param>
+        /// <returns>The index, or -1 if the key cannot be found and
+        /// <see cref="safe"/> is true.</returns>
+        /// <exception cref="Exception">Thrown when <see cref="safe"/> is False
+        /// and the key is not found.</exception>
         private int FindKeyIndex(TKey key, bool safe)
         {
             var tableLen = m_Table.Length;
@@ -96,6 +159,12 @@ namespace HashTable
             throw new Exception("Key Not Found!");
         }
 
+        /// <summary>
+        /// Call immediately after the entry at <see cref="indexKey"/> has been
+        /// removed to shift backward collided entries in the circular array.
+        /// </summary>
+        /// <param name="indexKey">The index of entry that has been removed.
+        /// </param>
         private void ShiftBackEntries(int indexKey)
         {
             int tableLen = m_Table.Length;
@@ -105,7 +174,7 @@ namespace HashTable
                 var index = (indexKey + i) % tableLen;
                 if (!m_Table[index].Occupied)
                 {
-                    break;
+                   break;
                 }
                 var hash = m_Table[index].Hash;
                 if (indexSwap < index)
@@ -128,11 +197,26 @@ namespace HashTable
             m_Table[indexSwap] = new Entry();
         }
 
+        /// <summary>
+        /// An entry in the circular array.
+        /// </summary>
         private struct Entry
         {
+            /// <summary>
+            /// True if the entry is occupied with a key, false otherwise.
+            /// </summary>
             public bool Occupied;
+            /// <summary>
+            /// The hash of the key, if the <see cref="Occupied"/> is true.
+            /// </summary>
             public int Hash;
+            /// <summary>
+            /// The key, if <see cref="Occupied"/> is true.
+            /// </summary>
             public TKey Key;
+            /// <summary>
+            /// The value, if <see cref="Occupied"/> is true.
+            /// </summary>
             public TValue Value;
         }
     }
